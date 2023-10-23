9d88cb12a2c2956b0bccd2e87d559c27fd096d22
manimonji <manirastgar78@gmail.com>
2023-10-23 12:44:16 +0330

    Search Feature And Database Setup

--- routes/authors.js
+++ routes/authors.js
@@ -1,16 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const Author = require("../models/author");
 
-router.get("/", (req, res) => {
-    res.render("authors/index");
+router.get("/", async (req, res) => {
+    let searchOptions = req.query.name && req.query.name !== "" ? {name: new RegExp(req.query.name, "i")} : {};
+    try {
+        const authors = await Author.find(searchOptions); 
+        res.render("authors/index", {authors: authors, searchOptions: req.query})
+    } catch {
+        res.redirect("/")
+    }
 });
 
 router.get("/new", (req, res) => {
-    res.render("authors/new");
+    res.render("authors/new", {author: new Author()});
 });
 
-router.post("/", (req, res) => {
-
+router.post("/", async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    });
+    try {
+        const newAuthor = await author.save();
+        res.redirect(`authors`);
+    } catch {
+        res.render("authors/new", {
+            author: author,
+            errorMessage: "Error creating author"
+        });
+    }
+    // author.save().then((newAuthor) => {
+    //     res.redirect(`authors`);
+    // }).catch((err) => {
+    //     res.render("authors/new", {
+    //         author: author,
+    //         errorMessage: "Error creating author"
+    //     });
+    // });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
@@ -14,14 +15,15 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts)
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false}))
 
-if (process.env.NODE_ENV == 'production') {
-    const mongoose = require("mongoose");
-    mongoose.connect(process.env.DATABASE_URL);
-    const db = mongoose.connection;
-    db.on("error", error => console.log(error));
-    db.once("open", () => console.log("YOOHOOOO"))
-}
+console.log(process.env.DATABASE_URL);
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on("error", error => console.log(error));
+db.once("open", () => console.log("YOOHOOOO"))
 
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
